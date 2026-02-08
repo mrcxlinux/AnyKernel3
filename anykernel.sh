241820d4a85d9ae435a193cae6ef2b16043c7606
陈华 <144014896+mrcxlinux@users.noreply.github.com>
2026-02-08 20:59:42 +0200

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -42,14 +42,19 @@ case $kernel_version in
 esac
 
 ui_print " "
-ui_print "$(echo '5YaF5qC45p6E5bu66ICFOiDlsI8g5bCPdw==' | base64 -d)"
-ui_print "$(echo 'S2VybmVsX2J1aWxkZXIgOiB4aWFveGlhb3c=' | base64 -d)"
+ui_print "chenhua13 Kernel | Oplus 6.6.89"
+ui_print "创作者 / Creator： 陈华 (@mrcxlinux)"
 ui_print " "
-ui_print "$(echo '56aB5q2i5pyq57uP6K645Y+v5LqM5qyh5L+u5pS55oiW6L2s5ZSu77yM6L+d6ICF5b+F56m2' | base64 -d)"
-ui_print "$(echo 'Tm8gdW5hdXRob3JpemVkIG1vZGlmaWNhdGlvbiBvciByZXNhbGUgYWxsb3dlZC4gVmlvbGF0b3JzIHdpbGwgYmUgcHJvc2VjdXRlZC4=' | base64 -d)"
+ui_print "特别感谢 cctv18 和 小小w，让这一切成为可能！"
+ui_print "加入我的 Telegram：@mrcsreleases | @mrcsdiscussion"
+ui_print "Special thanks to cctv18 and xiaoxiaow for making this possible!"
+ui_print "Join my telegram: @mrcsreleases | @mrcsdiscussion"
+
 ui_print " "
-ui_print "$(echo '5Yqg5YWl5oiR5Lus55qEVGVsZWdyYW3nvqTnu4QgOiBAZ2tpX2tlcm5lbHNfeGlhb3hpYW93' | base64 -d)"
-ui_print "$(echo 'Sm9pbiBPdXIgVGVsZWdyYW0gR3JvdXAhIDogQGdraV9rZXJuZWxzX3hpYW94aWFvdw==' | base64 -d)"
+ui_print "我的项目完全免费，我不接受任何捐赠！"
+ui_print "任何盗用并出售我作品的人都应该感到羞愧。"
+ui_print "My projects are entirely free and I do not accept donations!"
+ui_print "Anyone stealing from me and selling should be ashamed."
 ui_print " " "  -> ksu_supported: $ksu_supported"
 $ksu_supported || abort "  -> Non-GKI device, abort."
 
